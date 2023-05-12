38035e2a6b8da8ee1948445a8c47eccdef91f0d1
Kauan Kima <kauanalmeidalima1405@gmail.com>
2023-05-11 23:44:17 -0300

    Updated Project

--- script.js
+++ script.js
@@ -1,15 +1,55 @@
-const msgResultado = document.querySelector("#valor");
-const inputGasolina = document.getElementById("gasolina");
-const inputEtanol = document.getElementById("etanol");
+let inputGasolina;
+let inputEtanol;
+let msgResultado = document.getElementById("valor");
 
 function calcular() {
-  const gasolina = parseFloat(inputGasolina.value);
-  const etanol = parseFloat(inputEtanol.value);
-  const media = etanol / gasolina;
+  inputGasolina = parseFloat(document.getElementById("gasolina").value.replace(",", "."));
+  inputEtanol = parseFloat(document.getElementById("etanol").value.replace(",", "."));
 
-  if (media <= 0.7) {
-    msgResultado.innerHTML = "Etanol é mais vantajoso";
+  if (isNaN(inputGasolina) && isNaN(inputEtanol)) {
+    swal({
+      title: "Digite valores válidos para os preços dos combustíveis",
+      text: "Tente novamente",
+      icon: "error",
+    });
+    limpar();
+  } else if (isNaN(inputGasolina)) {
+    swal({
+      title: "Digite um valor válido para o preço da Gasolina",
+      text: "Tente novamente",
+      icon: "error",
+    });
+    document.getElementById("gasolina").value = "";
+  } else if (isNaN(inputEtanol)) {
+    swal({
+      title: "Digite um valor válido para o preço do Etanol",
+      text: "Tente novamente",
+      icon: "error",
+    });
+    document.getElementById("etanol").value = "";
+  }
+
+  const mediaPreco = inputEtanol / inputGasolina;
+
+  if (mediaPreco <= 0.7) {
+    msgResultado.value = "Etanol é mais vantajoso!";
   } else {
-    msgResultado.innerHTML = "Gasolina é mais vantajoso";
+    msgResultado.value = "Gasolina é mais vantajoso!";
   }
 }
+
+function limpar() {
+  document.getElementById("valor").value = "";
+  document.getElementById("gasolina").value = "";
+  document.getElementById("etanol").value = "";
+}
+
+const clearAll = document.getElementById("limpar");
+clearAll.addEventListener("click", () => {
+  limpar();
+});
+
+const calc = document.getElementById("calcular");
+calc.addEventListener("click", () => {
+  calcular();
+});
